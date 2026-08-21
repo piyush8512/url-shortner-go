294e784c4b62302ddcc2e0ae35d3ad071f60b876
piyush8512 <prajapatipiyush851@gmail.com>
2026-08-21 13:52:06 +0530

    feat(db): added database connection file 2

---
+++ backend/internal/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect(databaseURL string) (*sql.DB, error) {
+
+	conn, err := sql.Open("postgres", databaseURL)
+	if err != nil return nil, err
+
+	conn.SetMaxOpenConns(10)
+	conn.SetMaxIdleConns(5)
+	conn.SetConnMaxLifetime(30*time.Minute)
+
+	if err := conn.Ping(); err != nil return nil, err
+
+	return conn, nil
+
+}
